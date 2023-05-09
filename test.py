8c7eb2b941d779782894cc9152febaa9f3f720f2
Kohara <1k8aioon@gmail.com>
2023-05-09 12:47:01 +0900

    create Mojo repositories.

---
+++ test.py
@@ -0,0 +1,33 @@
+#class test():
+#def main(self):
+
+global num1, num2 
+num1 = int(input("num1 : "))
+num2 = int(input("num2 : "))
+
+res = input("yes or no")
+if res == "yes":
+    sub_struct(1, 2)
+    x = sub_struct()
+    print(x)
+elif res == "no":
+    sub_struct(4, 2)
+
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
+def sub_struct(n1, n2):
+    print(f"入力された文字は{n1}, {n2}です")
+    return (num1, num2)
